fcdb1a548aea8dff57c7321555ec087410a217e3
Bedelo Sunda <bsunda@bess-f2r4s3.clusters.42paris.fr>
2025-01-15 16:23:52 +0100

    2

---
+++ ex02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsunda <bsunda@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 15:19:32 by bsunda            #+#    #+#             */
+/*   Updated: 2025/01/15 16:22:57 by bsunda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+int	main(void){
+	std::string	str = "HI THIS IS BRAIN";
+	std::string* str_ptr = &str;
+	std::string& str_ref = str;
+
+	std::cout << &str << std::endl;
+	std::cout << &str_ptr << std::endl;
+	std::cout << &str_ref << std::endl;
+	
+	std::cout << str << std::endl;
+	std::cout << *str_ptr << std::endl;
+	std::cout << str_ref << std::endl;
+}
