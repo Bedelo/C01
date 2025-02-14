cd4811adc25e6c830ce0d023f8bb76603edf0554
Bedelo Sunda <bsunda@paul-f4Br6s8.clusters.42paris.fr>
2025-02-14 13:58:16 +0100

    clean

--- ex03/HumanA.hpp
+++ ex03/HumanA.hpp
@@ -6,12 +6,12 @@
 /*   By: bsunda <bsunda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 16:37:34 by bsunda            #+#    #+#             */
-/*   Updated: 2025/02/11 19:24:28 by bsunda           ###   ########.fr       */
+/*   Updated: 2025/02/14 13:57:55 by bsunda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef HUMAN_A_HPP
-#define HUMAN_A_HPP
+#ifndef HUMAN_A_H
+#define HUMAN_A_H
 
 #include "Weapon.hpp"
 #include <iostream>
--- ex03/HumanB.hpp
+++ ex03/HumanB.hpp
@@ -6,12 +6,12 @@
 /*   By: bsunda <bsunda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 16:37:42 by bsunda            #+#    #+#             */
-/*   Updated: 2025/02/11 19:24:39 by bsunda           ###   ########.fr       */
+/*   Updated: 2025/02/14 13:58:04 by bsunda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef HUMAN_B_HPP
-#define HUMAN_B_HPP
+#ifndef HUMAN_B_H
+#define HUMAN_B_H
 
 #include "Weapon.hpp"
 #include <iostream>
